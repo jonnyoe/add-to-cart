5b6d61c00efe8ffb6c3878a8a6ec89571bfea2ad
Jonny Olsen <jono4727@gmail.com>
2024-07-30 23:59:56 -0700

    added to do list

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+const toDoListInDB = ref(database, "toDoList")
+const connectedRef = ref(database, ".info/connected")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
+
+const inputFieldElTwo = document.getElementById("input-field-two")
+const addButtonElTwo = document.getElementById("add-button-two")
+const toDoListEl = document.getElementById("to-do-list")
+
+
+onValue(connectedRef, function (snapshot) {
+    if (snapshot.val() === true) {
+        console.log("Connected to the database.")
+    } else {
+        console.log("Disconnected from the database.")
+    }
+})
 
 function addItemToCart() {
     let inputValue = inputFieldEl.value
@@ -22,11 +37,28 @@ function addItemToCart() {
     }
 }
 
+function addItemToDo() {
+    let inputValue = inputFieldElTwo.value
+
+    if (inputValue) {
+        push(toDoListInDB, inputValue)
+        clearInputFieldEl()
+    }
+}
+
 addButtonEl.addEventListener("click", addItemToCart)
+
+addButtonElTwo.addEventListener("click", addItemToDo)
 
 inputFieldEl.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         addItemToCart()
+    }
+})
+
+inputFieldElTwo.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        addItemToDo()
     }
 })
 
@@ -45,15 +77,44 @@ onValue(shoppingListInDB, function (snapshot) {
         }
     } else {
         shoppingListEl.innerHTML = "No items here... yet"
+
     }
 })
+
+onValue(toDoListInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+
+        clearToDoListEl()
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemId = currentItem[0]
+            let currentItemValue = currentItem[1]
+
+            appendItemToToDoListEl(currentItem)
+        }
+    } else {
+        toDoListEl.innerHTML = "Plenty to do"
+    }
+})
+
+
 
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
 
+function clearToDoListEl() {
+    toDoListEl.innerHTML = ""
+}
+
 function clearInputFieldEl() {
     inputFieldEl.value = ""
+}
+
+function clearInputFieldElTwo() {
+    inputFieldElTwo.value = ""
 }
 
 function appendItemToShoppingListEl(item) {
@@ -71,4 +132,20 @@ function appendItemToShoppingListEl(item) {
     })
 
     shoppingListEl.append(newEl)
+}
+
+function appendItemToToDoListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function () {
+        let exactLocationOfItemInDB = ref(database, `toDoList/${itemId}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+
 }
